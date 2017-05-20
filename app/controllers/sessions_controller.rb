6e94f161c08caebb46a5c45b5e42328db41cef19
Dawson Graham <hdawsong@gmail.com>
2017-05-19 22:03:18 -0700

    Add errors and nav bar partial, and setup routes in controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,1 +1,19 @@
-sessions_controller.rb
+get '/sessions' do
+  erb :'/sessions/new'
+end
+
+post '/sessions' do
+  login
+  if logged_in?
+    session[:id]
+    redirect "/users/#{@user.id}"
+  else
+    @errors= ['Incorrect login information']
+    erb :'/sessions/new'
+  end
+end
+
+get '/logout' do
+  logout
+  redirect '/'
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post "/users" do
+  user = User.new(params[:user])
+  user.password= params[:password_string]
+  if user.save
+    login(user)
+    redirect "/users/#{user.id}"
+  else
+    status 422
+    @errors = user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
